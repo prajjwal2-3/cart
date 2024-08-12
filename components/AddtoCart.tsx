bcd0bf62945bd3cf95555104b093269e21c8132b
prajjwal2-3 <121849327+prajjwal2-3@users.noreply.github.com>
2024-08-12 16:45:45 +0530

    remove from cart and conditional rendering feature added

--- components/AddtoCart.tsx
+++ components/AddtoCart.tsx
@@ -4,14 +4,21 @@ import useCartStore from "@/store/cart";
 
 export default function AddtoCart({ id }: { id: number }) {
   const cart = useCartStore();
+  
+  const isInCart = cart.products.find((productId) => productId.id === id);
+
   return (
     <button
       className="w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600 transition-colors dark:bg-blue-600 dark:hover:bg-blue-700"
       onClick={() => {
-        cart.addProduct(id);
+        if (isInCart) {
+          cart.removeProduct(id); 
+        } else {
+          cart.addProduct(id);
+        }
       }}
     >
-      Add to Cart
+      {isInCart ? 'Remove from cart' : 'Add to Cart'}
     </button>
   );
 }
